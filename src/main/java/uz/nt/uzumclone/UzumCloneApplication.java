212aee0d283b5e59f694f30ea46c4f993380f228
YusupovAvazbek <yusupofavazbek@gmail.com>
2023-05-17 10:56:35 +0500

    pageges created

---
+++ src/main/java/uz/nt/uzumclone/UzumCloneApplication.java
@@ -0,0 +1,13 @@
+package uz.nt.uzumclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UzumCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UzumCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/nt/uzumclone/UzumCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.nt.uzumclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UzumCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
