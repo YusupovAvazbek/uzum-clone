3432affb5c13a097ff2816d35becb274b0134bf8
YusupovAvazbek <yusupofavazbek@gmail.com>
2023-06-05 13:10:35 +0500

    address resources added

--- src/main/java/uz/nt/uzumclone/rest/AddressResources.java
+++ src/main/java/uz/nt/uzumclone/rest/AddressResources.java
@@ -1,12 +1,32 @@
 package uz.nt.uzumclone.rest;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import uz.nt.uzumclone.dto.AddressDto;
+import uz.nt.uzumclone.dto.ResponseDto;
+import uz.nt.uzumclone.service.AddressService;
 
 @RestController
 @RequestMapping("/address")
 @RequiredArgsConstructor
 public class AddressResources {
+    private final AddressService addressService;
 
+    @PostMapping
+    public ResponseDto<AddressDto> addAddress(@RequestBody AddressDto addressDto) {
+        return addressService.add(addressDto);
+    }
+
+    @PatchMapping
+    public ResponseDto<AddressDto> updateAddress(@RequestBody AddressDto addressDto){
+        return addressService.update(addressDto);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseDto<Void> deleteAddress(@PathVariable Integer id){
+        return  addressService.delete(id);
+    }
+    @GetMapping("/{id}")
+    public ResponseDto<AddressDto> getById(@PathVariable Integer id){
+        return addressService.getById(id);
+    }
 }
